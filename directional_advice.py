0894e6f786dbe15c254466228779db5233501f22
dbrem <dbrems97@gmail.com>
2022-06-21 18:07:37 +0200

    Added docstrings, Signal emits

--- directional_advice.py
+++ directional_advice.py
@@ -1,16 +1,22 @@
-from PyQt5.QtWidgets import QSlider, QPushButton, QVBoxLayout, QHBoxLayout, QWidget, QLabel, \
-    QToolButton, QStyle
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtCore import Qt, QPointF, QRectF
+# -*- coding: utf-8 -*-
+"""
+Created on 15.04.2022
+
+Author: Daniel Brems
+"""
+
+from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QWidget, QLabel
+from PyQt5.QtCore import Qt, pyqtSignal
 from PyQt5 import QtGui, QtCore, QtWidgets
 
-from enum import Enum, IntEnum
+from enum import IntEnum
 
 # Todo: style with css
 import css
 
 
 class DisplayState(IntEnum):
+    """Defines the possible states for the widget"""
     DOUBLE_INC = 2
     INC = 1
     NORMAL = 0
@@ -19,8 +25,8 @@ class DisplayState(IntEnum):
 
 
 class _ArrowBox(QWidget):
-    def __init__(self, orientation, is_inc, color_1 = QtCore.Qt.darkBlue,
-                 color_2 = QtCore.Qt.blue, *args, **kwargs):
+    """Widget that acts as a canvas to draw the chevrons onto"""
+    def __init__(self, orientation, is_inc, color_1, color_2, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._orientation = orientation
         self.is_inc = is_inc
@@ -29,11 +35,13 @@ class _ArrowBox(QWidget):
         self._color_1 = color_1
         self._color_2 = color_2
 
+        # this makes the ArrowBox still use it's space, even when no chevron is drawn
         self.setSizePolicy(
             QtWidgets.QSizePolicy.MinimumExpanding,
             QtWidgets.QSizePolicy.MinimumExpanding
         )
 
+        # define offsets and rotations depending on the orientation of the whole widget
         if orientation == Qt.Horizontal:
             self.rotation = 0
             self.x_offset = 40
@@ -46,28 +54,26 @@ class _ArrowBox(QWidget):
     def sizeHint(self):
         return QtCore.QSize(100, 100)
 
-    def draw(self):
-        pass
-
     def setDouble(self, b):
+        """flag for indicating if double or single chevron should be drawn"""
         self._is_double = b
 
     def paintEvent(self, e):
-        # self.paint_triangle(e)
-        # self.paint_chevron(e)
-        self.paint_angle(e)
+        """overriding parents paint event for intelligent redrawing of the arrows. See Qt Docs"""
+        self.paint_angle()
 
-    def paint_angle(self, e):
+    def paint_angle(self):
+        """paints one or two chevrons with the respective orientation on the canvas"""
         painter = QtGui.QPainter(self)
         painter.begin(self)
-        painter.setRenderHint(QtGui.QPainter.Antialiasing)
 
-        # print(f'self.is_double = {self._is_double}')
+        # enable antialiasing to prevent artifacts ("Jaggies")
+        painter.setRenderHint(QtGui.QPainter.Antialiasing)
 
         pen = QtGui.QPen()
         pen.setColor(self._color_1)
-        pen.setWidth(15)
-        pen.setCapStyle(Qt.RoundCap)
+        pen.setWidth(12)
+        pen.setCapStyle(Qt.SquareCap)
         pen.setJoinStyle(Qt.MiterJoin)
 
         painter.setPen(pen)
@@ -190,8 +196,6 @@ class _ArrowBox(QWidget):
                 painter.drawLine(angleLine)
 
                 # second chevron second line
-
-
                 angleLine = QtCore.QLineF()
                 angleLine.setP1(QtCore.QPoint(xc - self.x_offset, yc - self.y_offset))
                 angleLine.setAngle(-45 + self.rotation)
@@ -207,139 +211,47 @@ class _ArrowBox(QWidget):
                 painter.drawLine(angleLine)
 
 
-    def draw_chevron(self, offset=0):
-        pass
-
-    def paint_chevron(self, e):
-        padding = 20
-        painter = QtGui.QPainter(self)
-        painter.begin(self)
-        painter.setRenderHint(QtGui.QPainter.Antialiasing)
-
-        pen = QtGui.QPen()
-        pen.setColor(self._color_1)
-        pen.setWidth(15)
-        pen.setCapStyle(Qt.RoundCap)
-        pen.setJoinStyle(Qt.MiterJoin)
-
-        painter.setPen(pen)
-
-        # define some points
-        width = painter.device().width()
-        height = painter.device().height()
-
-        p_width = width - padding
-        p_height = height - padding
-
-        xc = int(width / 2)  # x center
-        yc = int(height / 2)  # y center
-
-        # paint the chevron
-        # painter.drawLine(width - padding, 0 + padding, 0 + padding, painter.device().height() - padding)
-        painter.drawLine(xc, yc, p_width, padding)
-        pen.setColor(self._color_2)
-        painter.setPen(pen)
-        # painter.setBrush(QtCore.Qt.black)
-        painter.drawLine(xc, yc, p_width, p_height)
-
-    def paint_triangle(self, e):
-        painter = QtGui.QPainter(self)
-
-        # draw background
-        brush = QtGui.QBrush()
-        color = QtGui.QColor(0, 0, 0)
-        color.setNamedColor('#468faf')
-        brush.setColor(color)
-        brush.setStyle(Qt.SolidPattern)
-        rect = QtCore.QRect(0, 0, painter.device().width(), painter.device().height())
-        painter.fillRect(rect, brush)
-
-        padding = 3
-
-        # draw the triangle
-        # start_point = QPointF(*((0, painter.device().height()/2 - (padding * 2)) if self._orientation else (painter.device().width() - padding * 2, painter.device().height()/2 - (padding * 2))))        # start_point = QPointF(*((0, painter.device().height()/2 - (padding * 2)) if self._orientation else (painter.device().width() - padding * 2, painter.device().height()/2 - (padding * 2))))
-        start_point = QPointF(0 + 2 * padding, painter.device().height() / 2)
-        path = QtGui.QPainterPath(start_point)
-        painter.begin(self)
-        painter.setRenderHint(QtGui.QPainter.Antialiasing)
-        painter.setPen(Qt.black)
-        painter.setBrush(QtGui.QColor(0, 0, 200, 127))
-
-        path.lineTo(painter.device().width() - 2 * padding, painter.device().height() - 2 * padding)
-
-        path.lineTo(painter.device().width() - 2 * padding, 0 + 2 * padding)
-
-        path.lineTo(0 + 2 * padding, painter.device().height() / 2)
-
-        painter.drawPath(path)
-
-    # def paintEvent(self, e):
-    #     # if self.direction:
-    #     painter = QtGui.QPainter(self)
-    #     start_point = QPointF(*((0, painter.device().height()/2) if self._orientation else (painter.device().width(), painter.device().height()/2)))
-    #     path = QtGui.QPainterPath(start_point)
-    #     painter.begin(self)
-    #     painter.setRenderHint(QtGui.QPainter.Antialiasing)
-    #     painter.setPen(Qt.black)
-    #     painter.setBrush(QtCore.Qt.green)
-    #
-    #     path.lineTo(painter.device().width(), painter.device().height())
-    #
-    #     path.lineTo(painter.device().width(), 0)
-    #
-    #     path.lineTo(0, painter.device().height()/2)
-    #
-    #     painter.drawPath(path)
-
-    # def paintEvent(self, e):
-    #     painter = QtGui.QPainter(self)
-    #     target = QRectF(10.0, 20.0, 80.0, 60.0)
-    #     source = QRectF(0.0, 0.0, 1000.0, 1000.0)
-    #     pixmap = QtGui.QPixmap('right.bmp')
-
-    #       painter.drawPixmap(target, pixmap, source)
-
-    def set_icon(self, filename):
-        pass
-        pixmap = QtGui.QPixmap(filename)
-
-        # mask = pixmap.createMaskFromColor(QtGui.QColor('black'), Qt.MaskOutColor)
-        # # pixmap.fill((QtGui.QColor('red')))
-        # pixmap.setMask(mask)
-
-        # self.btNew = QPushButton('Servus', self)
-        self.btNew = QToolButton(self)
-        self.btNew.setIcon(QtGui.QIcon(pixmap))
-
-
 class DirectionWidget(QWidget):
-    def __init__(self, text='', orientation=Qt.Horizontal, state=DisplayState.NORMAL, *args,
-                 **kwargs):
+    """directional widget for indicating the user a increment or decrement in state"""
+    # signals need to be defined as class variables
+    textChanged = pyqtSignal(str)
+    stateChanged = pyqtSignal(DisplayState)
+
+    def __init__(self, text='', orientation=Qt.Horizontal, state=DisplayState.NORMAL, color_1=QtCore.Qt.darkBlue,
+                 color_2=QtCore.Qt.blue, background_color=None,  *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        # member variables
         self._state = state
         self._orientation = orientation
+        self._color_1 = color_1
+        self._color_2 = color_2
+        self._background_color = background_color
 
+        # sanity check for orientation
         if not isinstance(self._orientation, Qt.Orientation):
-            # TypeError: unable to convert a Python 'list' object to a C + + 'Qt::Orientation' instance
-            raise TypeError('Orientation must be passed as Qt::Orientation instance')
+            raise TypeError('unable to convert a Python ' + str(type(self._orientation)) +
+                            ' object to a C++ \'Qt::Orientation\' instance')
 
+        # choose layout according to orientation
         if self._orientation is Qt.Horizontal:
             self._layout = QHBoxLayout(self)
         else:
             self._layout = QVBoxLayout(self)
-            print('Vertical not yet implemented')
 
         self._lbl_current_state = QLabel(text, self)
         self._lbl_current_state.setMargin(5)
         self._lbl_current_state.setAlignment(Qt.AlignCenter)
 
-        self.inc_arrow = _ArrowBox(self._orientation, is_inc=True)
-        self.dec_arrow = _ArrowBox(self._orientation, is_inc=False)
+        # create two ArrowBox instances for displaying the chevrons
+        self.inc_arrow = _ArrowBox(self._orientation, is_inc=True, color_1=self._color_1, color_2=self._color_2)
+        self.dec_arrow = _ArrowBox(self._orientation, is_inc=False, color_1=self._color_1, color_2=self._color_2)
 
+        # first, dont show any chevrons by disabling updates for the ArrowBoxes
         self.inc_arrow.setUpdatesEnabled(False)
         self.dec_arrow.setUpdatesEnabled(False)
 
+        # choose widget order according to orientation
         if self._orientation == Qt.Horizontal:
             self._layout.addStretch()
             self._layout.addWidget(self.dec_arrow)
@@ -357,21 +269,26 @@ class DirectionWidget(QWidget):
         self._lbl_current_state.setStyleSheet(styleSheet)
 
     def setText(self, text):
+        """Set a new text for the center label of the widget"""
         self._lbl_current_state.setText(text)
+        self.textChanged.emit(text)
 
     def setState(self, state):
+        """set a new state for the widget. State must be of type DisplayState"""
         if not (isinstance(state, DisplayState)):
             raise TypeError('State must be an enum of type "DisplayState"')
         else:
             self._state = state
-            print(f'New State: {state}')
+
+            # emit a stateChange signal with the new state
+            self.stateChanged.emit(self._state)
             if self._state == DisplayState.DOUBLE_INC:
-                print('Drawing double inc')
+                # print('Drawing double inc')
                 self.inc_arrow.setDouble(True)
                 self.inc_arrow.setUpdatesEnabled(True)
                 self.dec_arrow.setUpdatesEnabled(False)
             if self._state == DisplayState.INC:
-                print('Drawing inc')
+                # print('Drawing inc')
                 self.inc_arrow.setDouble(False)
                 self.inc_arrow.setUpdatesEnabled(True)
                 self.dec_arrow.setUpdatesEnabled(False)
@@ -379,34 +296,35 @@ class DirectionWidget(QWidget):
                 self.dec_arrow.setUpdatesEnabled(False)
                 self.inc_arrow.setUpdatesEnabled(False)
             elif self._state == DisplayState.DOUBLE_DEC:
-                print('Drawing dec')
+                # print('Drawing dec')
                 self.dec_arrow.setDouble(True)
                 self.dec_arrow.setUpdatesEnabled(True)
                 self.inc_arrow.setUpdatesEnabled(False)
             elif self._state == DisplayState.DEC:
-                print('Drawing double dec')
+                # print('Drawing double dec')
                 self.dec_arrow.setDouble(False)
                 self.dec_arrow.setUpdatesEnabled(True)
                 self.inc_arrow.setUpdatesEnabled(False)
             self.update()
 
-
-    def set_color(self, color):
+    def setColor(self, color):
         # if not isinstance(color, ...)
         pass
 
-    def increment_state(self):
+    def increment(self):
+        """increment the current state by one. If max state is reached, nothing will happen."""
         s = self._state
         if s.value < 2:
-            new_state = DisplayState(s+ 1)
+            new_state = DisplayState(s + 1)
             self.setState(new_state)
 
-    def decrement_state(self):
+    def decrement(self):
+        """decrement the current state by one. If min state is reached, nothing will happen."""
         s = self._state
         if s.value > -2:
             new_state = DisplayState(self._state.value - 1)
             self.setState(new_state)
 
-    def clear_state(self):
-        # implement if time left
-        pass
+    def clear(self):
+        """clear the current state (no direction is shown)"""
+        self.setState(DisplayState.NORMAL)
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 15.04.2022
+
+Author: Daniel Brems
+"""
+
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QWidget, QPushButton
 from directional_advice import DirectionWidget, DisplayState
 from PyQt5.QtCore import Qt
-import css
 
 
 class MainWindow(QMainWindow):
@@ -16,17 +22,23 @@ class MainWindow(QMainWindow):
         self.central_layout = QVBoxLayout(self)
         self.main_widget.setLayout(self.central_layout)
 
-        self.widget = DirectionWidget(text='E', orientation=Qt.Horizontal, parent=self)
-        self.widget.setStyleSheet('font-size: 30px; font-weight: 700;'
-                                  ' background-color: rgb(13, 147, 214, 0.33);')
+        self.widget = DirectionWidget(text='Extra Extra Large Text', orientation=Qt.Horizontal, parent=self)
+        self.widget.setStyleSheet('font-size: 30px; font-weight: 700;')
 
-        # self.widget.setText('Hallo')
-        # self.widget.setState(DisplayState.NORMAL)
+        self.widget_2 = DirectionWidget(text='E', orientation=Qt.Vertical, parent=self)
+        self.widget_2.setStyleSheet('font-size: 30px; font-weight: 700;')
+
+        # self.widget.setText('1')
+        self.widget.setState(DisplayState.NORMAL)
+        self.central_layout.addWidget(self.widget_2)
         self.central_layout.addWidget(self.widget)
 
         self.btn_dec = QPushButton('Decrement')
         self.btn_clear = QPushButton('Clear')
         self.btn_inc = QPushButton('Increment')
+
+        # self.btn_text = QPushButton('Change Text')
+        # self.btn_text.clicked.connect(change_text)
 
         self.btn_dec.clicked.connect(self.decrement)
         self.btn_clear.clicked.connect(self.clear)
@@ -39,16 +51,33 @@ class MainWindow(QMainWindow):
 
         self.central_layout.addLayout(self.btn_layout)
 
+        self.widget.stateChanged.connect(self.print_new_state)
+        self.widget_2.stateChanged.connect(self.print_new_state)
+
+        self.widget.textChanged.connect(self.print_new_text)
+        self.widget_2.textChanged.connect(self.print_new_text)
+
+    # def change_text(self):
+    #     if self.widget.
+
+    def print_new_state(self, state):
+        print('New State: {}'.format(state))
+
+    def print_new_text(self, text):
+        print('New Text: {}'.format(text))
+
     def decrement(self):
-        # self.widget.setState(DisplayState.DOUBLE_DEC)
-        self.widget.decrement_state()
+        self.widget.decrement()
+        self.widget_2.decrement()
 
     def clear(self):
-        self.widget.setState(DisplayState.NORMAL)
+        self.widget.clear()
+        self.widget_2.clear()
 
     def increment(self):
-        # self.widget.setState(DisplayState.DOUBLE_INC)
-        self.widget.increment_state()
+        self.widget.increment()
+        self.widget_2.increment()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
